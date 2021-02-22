086d933965efd8fe42865fd7b2edaa35351c73e2
Alisson Leal <alissonsleal@hotmail.com>
2021-02-22 17:57:17 -0300

    Add Profile Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import ExperienceBar from '../components/ExperienceBar';
+import Main from '../components/Main';
 import Layout from '../Layout';
 
 export default function Home() {
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <Layout>
-        <ExperienceBar />
+        <Main />
       </Layout>
     </div>
   );
